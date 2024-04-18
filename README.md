4f7baeb295859f20b9551fb77ad9f5c30d976b1f
Fernando Villarreal <fernandovillarreal@mdp.edu.ar>
2024-04-18 16:40:08 -0300

    initial_version

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# SNIP
-Accesory script to remove sequences with non-IUPAC characters in multifasta files 
+- [SNIP](#snip)
+      - [Accesory script to remove Sequences with Non-IUPAC characters in multifasta file(s)](#accesory-script-to-remove-sequences-with-non-iupac-characters-in-multifasta-files)
+    - [Requirements](#requirements)
+    - [Single fasta file](#single-fasta-file)
+    - [Multiple fasta files](#multiple-fasta-files)
+
+
+#### Auxiliary script to remove Sequences with Non-IUPAC characters in multifasta file(s) 
+
+A common issue derived from poorly annotated genomes is that resulting proteomes may come with characters that do not correspond to actual amino acids commonly found in natural proteins (what we define here as non-IUPAC characters). 
+The most common scenario is the low definition of bases in the genome, typically annotated as nucleotide "N", for which translation of the predicted coding sequence may lead to an undefined residue, often times represented with symbol "X" in the protein sequence. 
+
+Some downstream applications (even MSA generation, profile to sequence comparisons, and even phylogeny reconstruction) can't deal with for non-IUPAC characters, and results are compromised. Moreover, some proteomes show a high "contamination" with non-IUPAC annotations, making this a problem that can easily become a nuissance. In addition, stop codon character * is typically found in complete proteome annotations, and can also interfere with several downstream applications.  
+
+In order to purge the protein datasets from sequences with non-IUPAC annotations, we have developed the Sequence Non-IUPAC Purge script, SNIP. SNIP is a python script that can deal with single or multiple fasta files, to automatically detect and remove sequences with non-IUPAC characters. SNIP do accept gap characters (such as "-" or "."), so either aligned or unaligned fasta files are accepted. The script will also process terminal stop codon character "*", and specifically remove it from the sequence. Note, however, that if a non terminal stop codon character is found in a sequence, it will be considered as non-IUPAC character, resulting in the removal of the sequence.
+
+>**We recommend applying this script before to run MuFasA and Seqrutinator.** 
+
+### Requirements
+
+SNIP requires Biopython, and accepts by argument either single (`-s`) or multiple (`-m`) fasta files. For multiple file, it is required that all of them have the same extension. 
+
+### Single fasta file
+The output will depend on the results. If running a single fasta file:
+
+`python3 SNIP.py -s input.fasta` 
+
+and sequences with non-IUPAC characters are found, two output fasta files will be generated: `input_accepted.fasta` and `input_removed.fasta`. Of course, each file includes the sequences without and with non-IUPAC characters, respectively. The terminal will print out the amount of sequences (total, accepted and removed). It will also show which is the non-IUPAC character found for each removed sequence.
+
+```
+Results for file input.fasta
+Total seqs:  65809
+Accepted seqs:  65368
+Removed seqs:  441
+```
+Note, in addition, that sequences in both files will not have terminal stop codon character "*".
+
+Now, if no sequences with non-IUPAC characters are found, but terminal stop codons are detected, the resulting sequences, without these characters, will be written in a new file with extension `_nsc.fasta` (from no stop codons).
+
+Finally, if neither non-IUPAC or terminal stop codon characters are found, only a printout in terminal is shown expliciting it (no further files are written). 
+
+### Multiple fasta files
+SNIP can be applied to multiple fasta files. The only requirements are that all files should (i) be in the same folder, and (ii) have the same extension. For example, can be run like this: 
+
+`python3 SNIP.py -m '*.fa`  
+
+in a folder with five fasta files with the provided extension:
+
+```
+SNIP.py
+sfa.fa
+crh.fa
+smu.fa
+tpl.fa
+cri.fa
+```
+
+Will produce two folders, `/Seqs_Accepted` and `/Seqs_Removed`, plus a summary file `Report_multiple_files.tsv`. The files generated are the same and created conditionally as described before. The difference is that all files `*_removed.fa` will be moved to the `/Seqs_Removed` folder (if any). The `*_accepted.fa` and `*_ncs.fa` (if any) are moved to `/Seqs_Accepted`, as well as all files that were not modified by the script. This is to simplify the output recovery for the user. 
+
+The `Report_multiple_files.tsv` summarizes the results, for example:
+
+| File           | Total_Seqs | Seqs_accepted | Seqs_removed | term_* |
+|----------------|------------|---------------|--------------|--------|
+| crh.fa         | 19527      | 19514         | 13           | yes    |
+| cri.fa | 75253      | 75253         | 0            | yes    |
+| sfa.fa | 45611      | 45611         | 0            | no     |
+| smu.fa | 27137      | 24567         | 2570         | no     |
+| tpl.fa | 65809      | 65368         | 441          | yes    |
+> The column term_* indicates if terminal stop codon characters were found
+
+The user can identify here that for input file `crh.fa` has 19527 seqs, of which 13 have non-IUPAC characters (and are removed), and terminal stop codon characters were found. Thus, `crh_accepted.fa` and `crh_removed.fa` files are created and saved in the proper folder. The same goes for `smu.fa` (note that around 10% of the sequences have non-IUPAC characters) and `tpl.fa`. 
+The case of `cri.fa` will result in a file `cri_nsc.fa`, since there were no sequences with non-IUPAC characters, but terminal stop codons were found. 
+Finally, a copy of `sfa.fa` will be found in the `/Seqs_Accepted` folder, since neither non-IUPAC or terminal stop codons were found. 
--- SNIP.py
+++ SNIP.py
@@ -3,42 +3,35 @@
 ###############################################################################
 #                         Script Description                                  #
 # name:                                                                       #
-#     bad_seqs_v01.py                                                         #
+#     SNIP.py                                                                 #
 #                                                                             #
 # description:                                                                #
-#     bad aminoacid detector , remove sequences with bad aminoacids           #
-#                                                                             #
+#     detects and removes sequences with non-IUPAC characters from multifasta #
+#     file(s)                                                                 #
 #                                                                             #
 # Authors:                                                                    #
-#     Agustin Amalfitano, Nico Stocchi                                        #
+#     Agustin Amalfitano, Nico Stocchi, Fernando Villarreal                   #
 #                                                                             #
 # Date:                                                                       #
-#     2019-10-22                                                              #
-# Modified for python 3 and output rename by Villarreal Fernando (04/11/2022) #
+#     2024-04-18                                                              #
 ###############################################################################
 
 from Bio import SeqIO
 import argparse
+import re
+import glob
+import os
 
+numbers = re.compile(r'(\d+)')
 
-# command line parameters
-parser = argparse.ArgumentParser()
+def numericalSort(value):
+    parts = numbers.split(value)
+    parts[1::2] = map(int, parts[1::2])
+    return parts
 
-# input fastafile
-parser.add_argument("-i", default="target.faa",
-                    help="Specify the fasta file (-i target.fsa) by def")
-#all parameters in args
-args = parser.parse_args()
-
-# redefine the parameters names
-fasta_file = args.i
-real_fasta = str((fasta_file.split(".")[:-1])[0])
-
-accepted_amino = ["A", "a", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G",
-    "h", "H", "i", "I", "k", "K", "l", "L", "m", "M", "n", "N", "p", "P", "q",
-    "Q", "r", "R", "s", "S", "t", "T", "v", "V", "w", "W", "y", "Y", "*", "-",
-    "."]
-
+def input_files(ftype):
+    list_of_files = sorted((glob.glob(ftype)), key=numericalSort)
+    return list_of_files
 
 def seqs_extractor(fasta_file):
     names = []
@@ -48,33 +41,153 @@ def seqs_extractor(fasta_file):
         seqs.append(seq_record.seq)
     return names, seqs
 
+# command line parameters
+parser = argparse.ArgumentParser()
 
-def fetching(hits, target, output):
+# input sinlge fasta file
+parser.add_argument("-s",
+                    help="Specify a single fasta file (target.faa). Accepts unaligned or aligned file.")
 
-    with open(output, "w") as f:
-        for seq_record in SeqIO.parse(target, "fasta"):
-            if seq_record.id in hits:
-                f.write(">" + str(seq_record.id) + "\n")
-                f.write(str(seq_record.seq) + "\n")
-    f.close()
-    return()
+parser.add_argument("-m", default="",
+                    help="Specify the extension for multiple fasta files (e.g. '*.fasta'). Accepts unaligned or aligned files.")
 
+#all parameters in args
+args = parser.parse_args()
 
-names, seqs = seqs_extractor(fasta_file)
-bad_seqs = list()
-for name, seq in zip(names, seqs):
-    for amino in seq:
-        if amino not in accepted_amino:
-            bad_seqs.append(name)
-            print ("seq ", name, "has the following bad character: ", amino)
-            break
+# redefine the parameters names
+single_file = args.s
+ext = args.m
 
-print (bad_seqs)
-good_seqs = [seq for seq in names if seq not in bad_seqs]
+accepted_amino = ["A", "a", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G",
+                  "h", "H", "i", "I", "k", "K", "l", "L", "m", "M", "n", "N", "p", "P", "q",
+                  "Q", "r", "R", "s", "S", "t", "T", "v", "V", "w", "W", "y", "Y", "-",
+                  "."]
 
+if ext != "" and single_file != None:
+    print("You can't specify both a single file and a list of files.")
+    print("Please, choose only -i (single file) or -m (multiple files).")
+    exit()
 
-print ("total seqs: ", len(names))
-print ("accepted seqs: ", len(good_seqs))
-print ("bad seqs: ", len(bad_seqs))
-fetching(bad_seqs, fasta_file, str(real_fasta) + "_removed.fsa")
-fetching(good_seqs, fasta_file, str(real_fasta) + "_accepted.fsa")
+if ext == "" and single_file != None:
+    real_fasta = str((single_file.split(".")[:-1])[0])
+    names, seqs = seqs_extractor(single_file)
+    # Preprocess sequences: remove trailing whitespace and asterisks
+    original_seqs = seqs.copy()
+    seqs = [seq.rstrip().rstrip('*') for seq in seqs]
+    changes_made = any(original_seq != processed_seq for original_seq, processed_seq in zip(original_seqs, seqs))
+
+    bad_seqs_n = []
+    bad_seqs_s = []
+    for name, seq in zip(names, seqs):
+        for amino in seq:
+            if amino not in accepted_amino:
+                bad_seqs_n.append(name)
+                bad_seqs_s.append(seq)
+                print("seq ", name, "has the following non-IUPAC character: ", amino)
+                break
+
+    good_seqs_n = [seq for seq in names if seq not in bad_seqs_n]
+    good_seqs_s = [seq for seq in seqs if seq not in bad_seqs_s]
+    
+    print(f'Results for file {single_file}')
+    print("Total seqs: ", len(names))
+    print("Accepted seqs: ", len(good_seqs_n))
+    print("Removed seqs: ", len(bad_seqs_n))
+   
+    if len(bad_seqs_n) > 0:
+        removed_file = str(real_fasta) + "_removed." + str((single_file.split(".")[-1]))
+        accepted_file = str(real_fasta) + "_accepted." + str((single_file.split(".")[-1]))
+       
+        f1 = open(removed_file, "w")
+        for name_b, seq_b in zip(bad_seqs_n, bad_seqs_s):
+            f1.write(">" + str(name_b) + "\n")
+            f1.write(str(seq_b) + "\n")
+        f1.close()
+
+        f2 = open(accepted_file, "w")
+        for name_g, seq_g in zip(good_seqs_n, good_seqs_s):
+            f2.write(">" + str(name_g) + "\n")
+            f2.write(str(seq_g) + "\n")
+        f2.close()
+
+    elif not changes_made: 
+        print("There are no sequences with non-IUPAC characters nor terminal stop codons characters in your fasta file.")
+    
+    else: 
+        print('There are no sequences with non-IUPAC characters in your fasta file, but terminal stop codons characters were found.')
+        accep_file_prepr = str(real_fasta) + "_nsc." + str((single_file.split(".")[-1]))
+        f3 = open(accep_file_prepr, "w")
+        for names_a, seqs_a in zip(names, seqs):
+            f3.write(">" + str(names_a) + "\n")
+            f3.write(str(seqs_a) + "\n")
+        f3.close()
+
+if ext != "" and single_file == None:
+    if not os.path.exists('Seqs_Accepted'):
+        os.mkdir('Seqs_Accepted') 
+    if not os.path.exists('Seqs_Removed'):
+        os.mkdir('Seqs_Removed')
+    list_of_fastas = input_files(ext)
+    if list_of_fastas == []:
+        print("No files found with the extension provided.")
+        exit()
+    out_f = open("Report_multiple_files.tsv", "w")
+    out_f.write("File\tTotal_Seqs\tSeqs_accepted\tSeqs_removed\tterm_*\n")
+    
+    for fasta in list_of_fastas:
+        real_fasta = str((fasta.split(".")[:-1])[0])
+        names, seqs = seqs_extractor(fasta)
+        # Preprocess sequences: remove trailing whitespace and asterisks
+        original_seqs = seqs.copy()
+        seqs = [seq.rstrip().rstrip('*') for seq in seqs]
+        changes_made = any(original_seq != processed_seq for original_seq, processed_seq in zip(original_seqs, seqs))
+
+        bad_seqs_n = []
+        bad_seqs_s = []
+        for name, seq in zip(names, seqs):
+            for amino in seq:
+                if amino not in accepted_amino:
+                    bad_seqs_n.append(name)
+                    bad_seqs_s.append(seq)
+                    print("seq ", name, "has the following non-IUPAC character: ", amino)
+                    break
+
+        good_seqs_n = [seq for seq in names if seq not in bad_seqs_n]
+        good_seqs_s = [seq for seq in seqs if seq not in bad_seqs_s]
+
+        print(f'Results for file {fasta}')
+        print("Total seqs: ", len(names))
+        print("Accepted seqs: ", len(good_seqs_n))
+        print("Removed seqs: ", len(bad_seqs_n))
+
+        if len(bad_seqs_n) > 0:
+            removed_file = "./Seqs_Removed/" + str(real_fasta) + "_removed." + str((fasta.split(".")[-1]))
+            accepted_file = "./Seqs_Accepted/" + str(real_fasta) + "_accepted." + str((fasta.split(".")[-1]))
+        
+            f1 = open(removed_file, "w")
+            for name_b, seq_b in zip(bad_seqs_n, bad_seqs_s):
+                f1.write(">" + str(name_b) + "\n")
+                f1.write(str(seq_b) + "\n")
+            f1.close()
+
+            f2 = open(accepted_file, "w")
+            for name_g, seq_g in zip(good_seqs_n, good_seqs_s):
+                f2.write(">" + str(name_g) + "\n")
+                f2.write(str(seq_g) + "\n")
+            f2.close()
+
+        elif not changes_made: 
+            print("There are no sequences with non-IUPAC characters nor terminal stop codons characters in your fasta file.")
+            os.system("cp " + fasta + " Seqs_Accepted/")
+        else: 
+            print('There are no sequences with non-IUPAC characters in your fasta file, but terminal stop codons characters were found.')
+            accep_file_prepr = "./Seqs_Accepted/" + str(real_fasta) + "_nsc." + str((fasta.split(".")[-1]))
+            f3 = open(accep_file_prepr, "w")
+            for names_a, seqs_a in zip(names, seqs):
+                f3.write(">" + str(names_a) + "\n")
+                f3.write(str(seqs_a) + "\n")
+            f3.close()
+        term_SC = "yes" if changes_made else "no"
+        out_f.write(str(fasta) + "\t" + str(len(names)) + "\t" + str(len(good_seqs_n)) + "\t" + str(len(bad_seqs_n)) + "\t" + term_SC + "\n")
+    
+    out_f.close()
